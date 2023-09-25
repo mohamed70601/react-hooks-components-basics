502dd6bf3d72e9839030ce341954a23c1ef931e4
Mohamed TCHOKETCH <mohamedtchoketch0@gmail.com>
2023-09-25 06:29:23 +0200

    Completed assignment

--- src/Comment.js
+++ src/Comment.js
@@ -4,4 +4,13 @@ function Comment() {
   return <div>Naturally, I agree with this article.</div>;
 }
 
+// const Comment = () => <div>Naturally, I agree with this article.</div>;
+
+// This is a class component
+// class Comment extends React.Component {
+//   render() {
+//     return <div>Naturally, I agree with this article.</div>;
+//   }
+// }
+
 export default Comment;
